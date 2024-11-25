d96e7d335b9054c2f3de6608905106514d5141a0
red2center <arf162434@gmail.com>
2024-11-25 12:46:09 +0000

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -5,14 +5,21 @@ set ZIP_FILE=%TARGET_DIR%\HelloRedAppCenter.zip
 set EXE_PATH=%TARGET_DIR%\HelloRedAppCenter.exe
 set START_MENU_SHORTCUT="%APPDATA%\Microsoft\Windows\Start Menu\Programs\HelloRedAppCenter.lnk"
 set DESKTOP_SHORTCUT="%USERPROFILE%\Desktop\HelloRedAppCenter.lnk"
+
 if not exist "%TARGET_DIR%" mkdir "%TARGET_DIR%"
+
 echo Indiriliyor...
 powershell -Command "Invoke-WebRequest -Uri %ZIP_URL% -OutFile %ZIP_FILE%"
+
 echo Cikartiliyor...
 powershell -Command "Expand-Archive -Path '%ZIP_FILE%' -DestinationPath '%TARGET_DIR%'"
+
 echo Kisayol olusturuluyor - Baslat Menusu...
-powershell -Command "$s=(New-Object -COM WScript.Shell).CreateShortcut('%START_MENU_SHORTCUT%');$s.TargetPath='%EXE_PATH%';$s.Save()"
+powershell -Command "$s=(New-Object -COM WScript.Shell).CreateShortcut('%START_MENU_SHORTCUT%');$s.TargetPath='%EXE_PATH%';$s.WorkingDirectory='%TARGET_DIR%';$s.Save()"
+
 echo Kisayol olusturuluyor - Masaustunde...
-powershell -Command "$s=(New-Object -COM WScript.Shell).CreateShortcut('%DESKTOP_SHORTCUT%');$s.TargetPath='%EXE_PATH%';$s.Save()"
+powershell -Command "$s=(New-Object -COM WScript.Shell).CreateShortcut('%DESKTOP_SHORTCUT%');$s.TargetPath='%EXE_PATH%';$s.WorkingDirectory='%TARGET_DIR%';$s.Save()"
+
 del "%ZIP_FILE%"
+
 echo Islem tamamlandi.
